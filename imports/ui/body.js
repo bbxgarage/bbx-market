3dffc7305dad43bf99d6634498999225795054f6
= <lucasfontesgaspareto@gmail.com>
2018-01-09 23:31:24 -0200

    temporary ui state

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,14 +1,27 @@
 import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
-    // show newest tasks at the app
-    return Tasks.find({}, { sort: { text: -1 } });
-  }
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // if hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort : { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  imcompleteTasks() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
 });
 
 Template.body.events({
@@ -28,5 +41,8 @@ Template.body.events({
 
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
